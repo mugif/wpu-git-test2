4d17f238b412c984769faa76c9766c492f102492
mugif <ikbal.giffari@gmail.com>
2020-09-03 09:50:34 +0700

    Menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus, obcaecati, cupiditate nam quasi nobis sit itaque veniam perferendis aliquid inventore doloremque accusantium! Velit amet minima officia mollitia facere minus? Nihil.</p>
 </body>
 </html>
